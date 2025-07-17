2a92032542a314681fe418ffe374c5c7a24ff7d7
Shane Meaney <122496312+smeaney11@users.noreply.github.com>
2025-07-16 22:16:57 -1000

    c

---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Contact } from '@/lib/validationSchemas';
+import { Card } from 'react-bootstrap';
+import Image from 'next/image';
+
+/* Renders a single Contact. See list/page.tsx. */
+const ContactCard = ({ contact }: { contact: Contact }) => (
+  <Card className="h-100">
+    <Card.Header>
+      <Image src={contact.image} width={75} height={75} alt="Contact Image" />
+      <Card.Title>
+        {contact.firstName}
+        &nbsp;
+        {contact.lastName}
+      </Card.Title>
+      <Card.Subtitle>{contact.address}</Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>{contact.description}</Card.Text>
+    </Card.Body>
+  </Card>
+);
+
+export default ContactCard;
