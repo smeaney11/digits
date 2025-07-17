861102168cddf092186a74a0f0b52b6836ef9425
Shane Meaney <122496312+smeaney11@users.noreply.github.com>
2025-07-16 22:28:13 -1000

    c

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -19,7 +19,7 @@ const ListPage = async () => {
     firstName: 'Philip',
     lastName: 'Johnson',
     address: 'POST 307, University of Hawaii',
-    image: 'https://github.com/philipmjohnson.png?size=100',
+    image: '',
     description: 'I am a Professor of Information and Computer Sciences at the University of Hawaii, Director ' +
     'of the Collaborative Software Development Laboratory, and the CEO of OpenPowerQuality.com.',
   },
@@ -27,7 +27,7 @@ const ListPage = async () => {
     firstName: 'Henri',
     lastName: 'Casanova',
     address: 'POST 307, University of Hawaii',
-    image: 'https://avatars0.githubusercontent.com/u/7494478?s=460&v=4',
+    image: '',
     description: 'I am originally from France. I maintain a list of reports from my surf sessions. I have proof ' +
       'that I ran the Hana relay with an actual Team.',
   },
@@ -35,7 +35,7 @@ const ListPage = async () => {
     firstName: 'Kim',
     lastName: 'Binsted',
     address: 'POST 307, University of Hawaii',
-    image: 'https://www.ics.hawaii.edu/wp-content/uploads/2013/08/kim_binsted-square-300x300.jpg',
+    image: '',
     description: 'Kim Binsted received her BSc in Physics at McGill (1991), and her PhD in Artificial Intelligence' +
       'from the University of Edinburgh (1996). Her thesis topic was the computational modeling and generation of ' +
       'punning riddles, and her program, JAPE (Joke Analysis and Production Engine), generated puns such as ' +
@@ -50,7 +50,9 @@ const ListPage = async () => {
             <h1 className="text-center">List Contacts</h1>
             <Row xs={1} md={2} lg={3} className="g-4" />
             {contacts.map((contact) => (
-              <ContactCard key={contact.firstName + contact.lastName} contact={contact} />
+              <Col key={contact.firstName + contact.lastName}>
+                <ContactCard contact={contact} />
+              </Col>
             ))}
           </Col>
         </Row>
